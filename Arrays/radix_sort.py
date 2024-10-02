8001d0ad2ba33f97c0d276b4d47dd83bd3f3f803
Aditya Gupta <aditya27gupta@gmail.com>
2024-10-02 23:45:32 +0530

    Added new algo

---
+++ Arrays/radix_sort.py
@@ -0,0 +1,8 @@
+"""
+Radix Sort Algorithm:
+    An array with non negative integers that needs to be sorted.
+    A two dimensional array with index 0 to 9 to hold values with the current radix in focus.
+    A loop that takes values from the unsorted array and places them in the correct position in the two dimensional radix array.
+    A loop that puts values back into the initial array from the radix array.
+    An outer loop that runs as many times as there are digits in the highest value.
+"""
